2cf27c6d15f616d172680e04dd2303006f2e4bf5
Ole J. Rosendahl <ole.johnny.rosendahl@gmail.com>
2015-09-02 16:17:25 +0700

    Added button API for default button

--- app/helpers/buttons_for/buttons_for_helper.rb
+++ app/helpers/buttons_for/buttons_for_helper.rb
@@ -1,4 +1,34 @@
 module ButtonsFor
   module ButtonsForHelper
+
+    def buttons_for(object, &block)
+      capture ButtonsForBuilder.new(object, self), &block
+    end
+
+    class ButtonsForBuilder
+      include ActionView::Helpers
+
+      attr_reader :object, :template
+
+      def initialize(object, template)
+        @object, @template = object, template
+      end
+
+      def button(text, url, options = {}, &block)
+        link_to label(text), url, class: classes(options)
+      end
+
+      private
+
+      def label(text)
+        text.capitalize
+      end
+
+      def classes(options)
+        "btn".tap do |s|
+        end
+      end
+    end
+
   end
 end
--- spec/helpers/buttons_for/buttons_for_helper_spec.rb
+++ spec/helpers/buttons_for/buttons_for_helper_spec.rb
@@ -1,6 +1,20 @@
 require "spec_helper"
 
 describe ButtonsFor::ButtonsForHelper do
-  it "does something" do
+  describe ButtonsFor::ButtonsForHelper::ButtonsForBuilder do
+    let(:object) { Struct.new(:name).new("Project 1") }
+
+    def builder
+      described_class.new(object, ActionView::Base.new)
+    end
+
+    describe "#button" do
+      it "generates a default button" do
+        expect(builder.button(:new, "#")).to eq(
+          "<a class=\"btn\" href=\"#\">New</a>"
+        )
+      end
+    end
+
   end
 end
