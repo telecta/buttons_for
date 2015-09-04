5fb547ec2a37e1d5db972d8d46f6e50524b30477
Ole J. Rosendahl <ole.johnny.rosendahl@gmail.com>
2015-09-04 16:56:14 +0700

    Rewrote tests to use assert_select instead for assert_equal

--- test/buttons_for_helper_test.rb
+++ test/buttons_for_helper_test.rb
@@ -2,26 +2,34 @@ require "test_helper"
 
 class ButtonsFor::Rails::ButtonsForHelperTest < ActionView::TestCase
 
+  def with_concat_buttons_for(*args, &block)
+    concat buttons_for(*args, &block)
+  end
+
   test "#button" do
-    assert_equal "<a class=\"btn\" title=\"Nuevo\" href=\"#\">Nuevo</a>", buttons_for(Object.new) { |b| b.button "Nuevo", "#" }
+    with_concat_buttons_for(Object.new) { |b| b.button "Nuevo", "#" }
+    assert_select "a.btn[href=\"#\"][title=\"Nuevo\"]", text: "Nuevo"
   end
 
   test "#new" do
-    assert_equal "<a class=\"btn btn-success\" title=\"Nuevo\" href=\"#\">" +
-      "<i class=\"fa fa-plus\"></i> Nuevo" +
-    "</a>", buttons_for(Object.new) { |b| b.new "#" }
+    with_concat_buttons_for(Object.new) { |b| b.new "#" }
+    assert_select "a.btn.btn-success[href=\"#\"][title=\"Nuevo\"]", text: "Nuevo" do |element|
+      assert_select element, "i.fa.fa-plus"
+    end
   end
 
   test "#edit" do
-    assert_equal "<a class=\"btn\" title=\"Editar\" href=\"#\">" +
-      "<i class=\"fa fa-pencil\"></i> Editar" +
-    "</a>", buttons_for(Object.new) { |b| b.edit("#") }
+    with_concat_buttons_for(Object.new) { |b| b.edit "#" }
+    assert_select "a.btn.btn[href=\"#\"][title=\"Editar\"]", text: "Editar" do |element|
+      assert_select element, "i.fa.fa-pencil"
+    end
   end
 
   test "#delete" do
-    assert_equal "<a class=\"btn btn-danger\" data-confirm=\"¿Estas seguro?\" title=\"Borrar\" rel=\"nofollow\" data-method=\"delete\" href=\"#\">" +
-      "<i class=\"fa fa-trash-o\"></i> Borrar" +
-    "</a>", buttons_for(Object.new) { |b| b.delete("#") }
+    with_concat_buttons_for(Object.new) { |b| b.delete "#" }
+    assert_select "a.btn.btn[href=\"#\"][title=\"Borrar\"][data-confirm=\"¿Estas seguro?\"]", text: "Borrar" do |element|
+      assert_select element, "i.fa.fa-trash-o"
+    end
   end
 
 end
