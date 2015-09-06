6af14debff38b54b3ca580660eb5bc518727da8a
Ole J. Rosendahl <ole.johnny.rosendahl@gmail.com>
2015-09-06 14:48:44 +0700

    Fix translation examples

--- test/buttons_for_helper_test.rb
+++ test/buttons_for_helper_test.rb
@@ -7,27 +7,27 @@ class ButtonsFor::Rails::ButtonsForHelperTest < ActionView::TestCase
   end
 
   test "#button" do
-    with_concat_buttons_for(Object.new) { |b| b.button "Nuevo", "#" }
-    assert_select "a.btn[href=\"#\"][title=\"Nuevo\"]", text: "Nuevo"
+    with_concat_buttons_for(Object.new) { |b| b.button "New", "#" }
+    assert_select "a.btn[href=\"#\"][title=\"New\"]", text: "New"
   end
 
   test "#new" do
     with_concat_buttons_for(Object.new) { |b| b.new "#" }
-    assert_select "a.btn.btn-success[href=\"#\"][title=\"Nuevo\"]", text: "Nuevo" do |element|
+    assert_select "a.btn.btn-success[href=\"#\"][title=\"New\"]", text: "New" do |element|
       assert_select element, "i.fa.fa-plus"
     end
   end
 
   test "#edit" do
     with_concat_buttons_for(Object.new) { |b| b.edit "#" }
-    assert_select "a.btn.btn[href=\"#\"][title=\"Editar\"]", text: "Editar" do |element|
+    assert_select "a.btn.btn[href=\"#\"][title=\"Edit\"]", text: "Edit" do |element|
       assert_select element, "i.fa.fa-pencil"
     end
   end
 
   test "#delete" do
     with_concat_buttons_for(Object.new) { |b| b.delete "#" }
-    assert_select "a.btn.btn[href=\"#\"][title=\"Borrar\"][data-confirm=\"¿Estas seguro?\"]", text: "Borrar" do |element|
+    assert_select "a.btn.btn[href=\"#\"][title=\"Delete\"][data-confirm=\"Are you sure?\"]", text: "Delete" do |element|
       assert_select element, "i.fa.fa-trash-o"
     end
   end
